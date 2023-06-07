60ca4780892b6661e469a740cd5a2f212ee7e3b9
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2023-06-07 18:31:32 +0200

    finished Sardinia example
    NASA rasters are a bitt odd; don't interpret the results. This was just a test

---
+++ functions/ndvi-palette.R
@@ -0,0 +1,3 @@
+create_ndvi_pal <- colorRampPalette(c('darkblue', 'dodgerblue', '#744700', '#d9bb94',
+                                      'darkgreen'))
+ndvi_pal <- create_ndvi_pal(100)
---
+++ functions/scale-ndvi.R
@@ -0,0 +1,3 @@
+ndvi_to_01 <- function(x) (x + 1) / 2
+
+ndvi_to_11 <- function(x) x * 2 - 1
