81d718372935e2ab938c8f4288bcbe03c7b3a278
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2025-03-21 17:25:10 -0700

    added NDVI palette

--- analysis/figures/default-ggplot-theme.R
+++ analysis/figures/default-ggplot-theme.R
@@ -4,3 +4,10 @@ theme_set(theme_bw() +
             theme(panel.grid = element_blank(),
                   text = element_text(face = 'bold')))
 
+
+# custom NDVI color palette
+create_ndvi_pal <- colorRampPalette(c('darkblue', 'dodgerblue', '#744700',
+                                      '#d9bb94', 'darkgreen'))
+ndvi_pal <- create_ndvi_pal(100)
+
+if(FALSE) khroma::plot_scheme_colorblind(ndvi_pal)
