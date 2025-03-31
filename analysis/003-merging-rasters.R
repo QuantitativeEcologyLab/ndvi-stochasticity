124dda932e8c20604044f7119300b5b0e5749416
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2025-03-31 14:05:10 -0700

    merged rasters; made subsets for 2020 and 2021

--- analysis/003-merging-rasters.R
+++ analysis/003-merging-rasters.R
@@ -32,6 +32,7 @@ if(FALSE) {
 ecoregions <- st_read('data/world-ecosystems/data/commondata/data0/tnc_terr_ecoregions.shp') %>%
   filter(WWF_REALM2 != 'Antarctic') # drop Antarctica
 
+# all rasters have the same CRS
 # check crs for first, last, and a random sample of rasters
 c(file_names[1], sample(file_names, size = 100), file_names[length(file_names)]) %>%
   map_chr(function(.fn) { # fast enough that it is not worth parallelizing
@@ -39,7 +40,9 @@ c(file_names[1], sample(file_names, size = 100), file_names[length(file_names)])
   }) %>%
   unique()
 
-ecoregions <- st_transform(ecoregions, crs(rast(file_names[1])))
+ecoregions <- st_transform(ecoregions, crs(rast(file_names[1]))) %>%
+  st_geometry() %>%
+  vect()
 
 # create a data frame of all dates
 dates <-
@@ -72,38 +75,127 @@ dates %>%
 # some dates are missing a raster (not available on the server)
 all(! is.na(dates$file_name))
 mean(! is.na(dates$file_name))
+filter(dates, is.na(file_name))
 
-# need to aggregate temporally to reduce file size before saving
+# need to aggregate temporally to reduce file size before saving ----
 # spatRast objects cannot be serialized (i.e., run in parallel): https://stackoverflow.com/questions/67445883/terra-package-returns-error-when-try-to-run-parallel-operations/67449818#67449818
+# data for 2020 omly ----
+tictoc::tic() # ~8 minutes on EME Linux
+dates_aggr_2020 <-
+  dates %>%
+  filter(! is.na(file_name)) %>% # drop missing rasters
+  filter(lubridate::year(date) == 2020) %>%
+  nest(cluster = ! group) %>%
+  mutate(central_date = map_dbl(cluster, function(.cl) {
+    mean(.cl$date)
+  }, .progress = 'Getting mean dates') %>%
+    as.Date(), # convert numeric back to date
+  ndvi_rast = map(cluster, function(.cl) {
+    map(.cl$file_name, \(.fn) {
+      rast(.fn, lyr = 'NDVI')
+    }) %>%
+      rast() %>%
+      mask(ecoregions) %>% # drop antarctica and unnecessary pixels
+      mean(na.rm = TRUE) %>%
+      return()
+  }, .progress = 'Calculating mean raster'),
+  n_rasters = map_int(cluster, nrow), # find number of rasters per group
+  ndvi_rast = map(ndvi_rast, \(x) {
+    # aggregating to ensure the final number of rows is below the max
+    terra::aggregate(x, 2) %>%
+      as.data.frame(xy = TRUE)
+  }, .progress = 'Aggregating rasters and converting to data frame')) %>%
+  select(group, central_date, ndvi_rast) %>%
+  unnest(ndvi_rast) %>%
+  rename(ndvi_15_day_mean = mean)
+
+saveRDS(dates_aggr_2020, 'data/ndvi-global-15-day-average-2020-only.rds')
+tictoc::toc()
+
+if(FALSE) {
+  # check the spatial distribution of the points
+  range(dates_aggr_2020$x)
+  range(dates_aggr_2020$y)
+  
+  # good coverage worldwide
+  dates_aggr_2020 %>%
+    slice_sample(n = 1e5) %>%
+    plot(y ~ x, .)
+  
+  # many days don't have much data on the north pole
+  dates_aggr_2020 %>%
+    filter(central_date == first(central_date)) %>%
+    slice_sample(n = 1e5) %>%
+    plot(y ~ x, .)
+}
+
+# data for 2021 omly ----
+tictoc::tic() # ~6 minutes on EME Linux
+dates_aggr_2021 <-
+  dates %>%
+  filter(! is.na(file_name)) %>% # drop missing rasters
+  filter(lubridate::year(date) == 2021) %>%
+  nest(cluster = ! group) %>%
+  mutate(central_date = map_dbl(cluster, function(.cl) {
+    mean(.cl$date)
+  }, .progress = 'Getting mean dates') %>%
+    as.Date(), # convert numeric back to date
+  ndvi_rast = map(cluster, function(.cl) {
+    map(.cl$file_name, \(.fn) {
+      rast(.fn, lyr = 'NDVI')
+    }) %>%
+      rast() %>%
+      mask(ecoregions) %>% # drop antarctica and unnecessary pixels
+      mean(na.rm = TRUE) %>%
+      return()
+  }, .progress = 'Calculating mean raster'),
+  n_rasters = map_int(cluster, nrow), # find number of rasters per group
+  ndvi_rast = map(ndvi_rast, \(x) {
+    # aggregating to ensure the final number of rows is below the max
+    terra::aggregate(x, 2) %>%
+      as.data.frame(xy = TRUE)
+  }, .progress = 'Aggregating rasters and converting to data frame')) %>%
+  select(group, central_date, ndvi_rast) %>%
+  unnest(ndvi_rast) %>%
+  rename(ndvi_15_day_mean = mean)
+
+saveRDS(dates_aggr_2021, 'data/ndvi-global-15-day-average-2021-only.rds')
+tictoc::toc()
+
+
+# all data (~7.55 hours on EME Linux) ----
+tictoc::tic()
 dates_aggr <-
   dates %>%
   filter(! is.na(file_name)) %>% # drop missing rasters
   nest(cluster = ! group) %>%
   mutate(central_date = map_dbl(cluster, function(.cl) {
     mean(.cl$date)
-  }) %>%
+  }, .progress = 'Getting mean dates') %>%
     as.Date(), # convert numeric back to date
   ndvi_rast = map(cluster, function(.cl) {
     map(.cl$file_name, \(.fn) {
-      rast(.fn, lyr = rep('NDVI', nrow(.cl)))
+      rast(.fn, lyr = 'NDVI')
     }) %>%
       rast() %>%
+      mask(ecoregions) %>% # drop antarctica and unnecessary pixels
       mean(na.rm = TRUE) %>%
       return()
-  }),
+  }, .progress = 'Calculating mean raster'),
   n_rasters = map_int(cluster, nrow), # find number of rasters per group
   ndvi_rast = map(ndvi_rast, \(x) {
     # aggregating to ensure the final number of rows is below the max
     terra::aggregate(x, 2) %>%
       as.data.frame(xy = TRUE)
-  })) %>%
+  }, .progress = 'Aggregating rasters')) %>%
   select(group, central_date, ndvi_rast) %>%
   unnest(ndvi_rast) %>%
   rename(ndvi_15_day_mean = mean)
 
 saveRDS(dates_aggr, 'data/ndvi-global-15-day-average.rds')
+tictoc::toc()
 
-# check the first raster
+# check the first raster ----
 if(FALSE) {
   library('ggplot2')
   source('analysis/figures/default-ggplot-theme.R')
