8a2d22e2894ebe034483d18a1f173800f294ffcf
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2025-04-24 09:37:50 -0700

    switched spatial smooths to sos basis

--- analysis/011-mean-ndvi-hgam.R
+++ analysis/011-mean-ndvi-hgam.R
@@ -10,8 +10,9 @@ source('functions/add_nb.R')
 ecoregions <- st_read('data/ecoregions/ecoregions-polygons.shp')
 
 # set up the data for the model
+#' *need to re-run to fix elevations*
 if(file.exists('data/hbam-ndvi-data.rds')) {
-  d <- readRDS('data/hbam-ndvi-data.rds')
+  d <- readRDS('data/hbam-ndvi-data-only-years-multiples-of-5.rds') #' *change*
 } else {
   # resolution is (0.05 degrees)^2 = (3 degree minutes)^2
   # create raster of ecoregion
@@ -27,7 +28,8 @@ if(file.exists('data/hbam-ndvi-data.rds')) {
   r_dist <- rast('data/distance-from-coast-m.tif')
   
   # takes a few minutes to import
-  d0 <- readRDS('data/ndvi-global-15-day-average.rds')
+  d0 <- readRDS('data/ndvi-global-15-day-average.rds') %>%
+    filter(lubridate::year(central_date) %% 5 == 0) #' *DELETE*
   
   tictoc::tic()
   #' `exactextractr::exact_extract()` fails due to bad class for coords
@@ -53,7 +55,7 @@ if(file.exists('data/hbam-ndvi-data.rds')) {
   if(FALSE) {
     plot(st_geometry(ecoregions))
     d %>%
-      filter(elevation_m < -20) %>%
+      filter(elevation_m < -0) %>%
       points(y ~ x, ., col = 'red', pch = 19)
   }
   
@@ -69,73 +71,75 @@ if(file.exists('data/hbam-ndvi-data.rds')) {
   gc()
 }
 
-# create list of neighbors ----
-# drop polygons with no data or neighbors
-ecoregions$in_data <- ecoregions$poly_id %in% unique(d$poly_id)
-
-filter(ecoregions, ! in_data)
-mean(! ecoregions$in_data)
-with(ecoregions, sum((! in_data) * area_km2) / sum(area_km2))
-max(ecoregions$area_km2[ecoregions$in_data])
-
-if(FALSE) {
-  ggplot(ecoregions, aes(log10(area_km2), in_data)) + geom_point()
-  
-  ggplot(ecoregions, aes(log10(area_km2), n_neigh)) +
-    facet_wrap(~ in_data) +
-    geom_point()
-}
-
-#' neighbors are not guaranteed to have data, so not using `n_neigh`
-ecoregions <- filter(ecoregions, in_data)
-
-if(file.exists('data/ecoregions/poly-nbs-global.rds')) {
-  nbs <- readRDS('data/ecoregions/poly-nbs-global.rds')
-} else {
-  nbs <-
-    spdep::poly2nb(pl = ecoregions,
-                   row.names = ecoreions$poly_id, # so that names match IDs
-                   queen = TRUE) # 1 point in common is sufficient
-  length(nbs) == nrow(ecoregions)
-  names(nbs) <- ecoregions$poly_id # names do not need to match indices
-  
-  # add missing neighbors because of polygons that cross the edge of the map
-  c('poly 8225', 'poly 8038', 'poly 7987', 'poly 8226', 'poly 7988', 'poly 8040',
-    'poly 8980', 'poly 8987') %in%
-    ecoregions$poly_id
-  
-  layout(matrix(1:6, ncol = 2))
-  # 7987 and 8038 are already neighbors
-  # 7988 and 8040 are already neighbors
-  # 7987 and 8226 do not touch
-  add_nb(p1 = 'poly 8225', p2 = 'poly 8226', add = TRUE)
-  add_nb(p1 = 'poly 8038', p2 = 'poly 8040', add = TRUE)
-  add_nb(p1 = 'poly 7987', p2 = 'poly 7988', add = TRUE)
-  add_nb(p1 = 'poly 7987', p2 = 'poly 8040', add = TRUE)
-  add_nb(p1 = 'poly 7988', p2 = 'poly 8038', add = TRUE)
-  # add_nb(p1 = 'poly 8980', p2 = 'poly 8987', add = TRUE) # not in data
-  layout(1)
-  saveRDS(nbs, 'data/ecoregions/poly-nbs-global.rds')
-}
+#' # create list of neighbors ----
+#' # drop polygons with no data or neighbors
+#' ecoregions$in_data <- ecoregions$poly_id %in% unique(d$poly_id)
+#' 
+#' filter(ecoregions, ! in_data)
+#' mean(! ecoregions$in_data)
+#' with(ecoregions, sum((! in_data) * area_km2) / sum(area_km2))
+#' max(ecoregions$area_km2[ecoregions$in_data])
+#' 
+#' if(FALSE) {
+#'   ggplot(ecoregions, aes(log10(area_km2), in_data)) + geom_point()
+#'   
+#'   ggplot(ecoregions, aes(log10(area_km2), n_neigh)) +
+#'     facet_wrap(~ in_data) +
+#'     geom_point()
+#' }
+#' 
+#' #' neighbors are not guaranteed to have data, so not using `n_neigh`
+#' ecoregions <- filter(ecoregions, in_data)
+#' 
+#' if(file.exists('data/ecoregions/poly-nbs-global.rds')) {
+#'   nbs <- readRDS('data/ecoregions/poly-nbs-global.rds')
+#' } else {
+#'   nbs <-
+#'     spdep::poly2nb(pl = ecoregions,
+#'                    row.names = ecoreions$poly_id, # so that names match IDs
+#'                    queen = TRUE) # 1 point in common is sufficient
+#'   length(nbs) == nrow(ecoregions)
+#'   names(nbs) <- ecoregions$poly_id # names do not need to match indices
+#'   
+#'   # add missing neighbors because of polygons that cross the edge of the map
+#'   c('poly 8225', 'poly 8038', 'poly 7987', 'poly 8226', 'poly 7988', 'poly 8040',
+#'     'poly 8980', 'poly 8987') %in%
+#'     ecoregions$poly_id
+#'   
+#'   layout(matrix(1:6, ncol = 2))
+#'   # 7987 and 8038 are already neighbors
+#'   # 7988 and 8040 are already neighbors
+#'   # 7987 and 8226 do not touch
+#'   add_nb(p1 = 'poly 8225', p2 = 'poly 8226', add = TRUE)
+#'   add_nb(p1 = 'poly 8038', p2 = 'poly 8040', add = TRUE)
+#'   add_nb(p1 = 'poly 7987', p2 = 'poly 7988', add = TRUE)
+#'   add_nb(p1 = 'poly 7987', p2 = 'poly 8040', add = TRUE)
+#'   add_nb(p1 = 'poly 7988', p2 = 'poly 8038', add = TRUE)
+#'   # add_nb(p1 = 'poly 8980', p2 = 'poly 8987', add = TRUE) # not in data
+#'   layout(1)
+#'   saveRDS(nbs, 'data/ecoregions/poly-nbs-global.rds')
+#' }
 
 # ensure names match the factor levels and the unique values
-d <- mutate(d, poly_id = factor(poly_id, levels = names(nbs)))
+# d <- mutate(d, poly_id = factor(poly_id, levels = names(nbs)))
 all.equal(sort(names(nbs)), sort(levels(d$poly_id)))
 all(levels(d$poly_id) %in% unique(d$poly_id))
 
 if(file.exists('models/global-models/hbam-mean-ndvi-DATE.rds')) {
-  m <- readRDS('models/global-models/hbam-mean-ndvi-NO-MRF-NO-TI-THINNED-DATA-2025-04-14.rds')
+  m_mu <- readRDS('models/global-models/hbam-mean-ndvi-NO-MRF-NO-TI-THINNED-DATA-2025-04-14.rds')
 } else {
-  DATE <- format(Sys.Date(), '%Y-%m-%d')
+  DATE <- paste0('mod-5-no-res-', format(Sys.Date(), '%Y-%m-%d'))
   
   # reduce memory impact
   if(Sys.info()['sysname'] == 'Windows') {
     d <- d %>%
-      select(ndvi_15_day_mean, biome, doy, year, elevation_m, poly_id) %>%
-      filter(year %% 2 == 1) %>%
+      select(ndvi_15_day_mean, doy, year, elevation_m, # biome,
+             x, y, central_date) %>%
+      # filter(year %% 5 == 0) %>%
+      slice(seq(1, n(), by = 50)) %>%
       mutate(doy = as.integer(doy),
              year = as.integer(year))
-    rm(ecoregions, m)
+    rm(ecoregions, m_mu)
   }
   gc()
   
@@ -143,10 +147,9 @@ if(file.exists('models/global-models/hbam-mean-ndvi-DATE.rds')) {
   # on lab windows PC: no ti, no mrf, and only odd years: 1.9 hours
   # on lab windows PC: no mrf and only odd years: 2.7 hours
   # on lab windows PC: only odd years and sos instead of mrf: XXX hours
-  m <- bam(
+  m_mu <- bam(
     ndvi_15_day_mean ~
       biome + # to avoid intercept shrinkage
-      s(x, y, bs = 'sos', k = 1e3) +
       s(doy, biome, bs = 'fs', xt = list(bs = 'cc'), k = 10) +
       s(year, biome, bs = 'fs', xt = list(bs = 'cr'), k = 10) +
       ti(doy, year, biome, bs = c('cc', 'cr', 're'), k = c(5, 5)) +
@@ -161,56 +164,87 @@ if(file.exists('models/global-models/hbam-mean-ndvi-DATE.rds')) {
     nthreads = future::availableCores(logical = FALSE) - 2,
     control = gam.control(trace = TRUE))
   
-  saveRDS(m, paste0('models/global-models/hbam-mean-ndvi-THINNED-DATA-', DATE, '.rds'))
+  saveRDS(m_mu, paste0('models/global-models/hbam-mean-ndvi-', DATE, '.rds'))
+  
+  # model with splines on the sphere
+  m_mu <- bam(
+    ndvi_15_day_mean ~
+      s(doy, bs = 'cc', k = 10) +
+      s(year, bs = 'cr', k = 9) +
+      s(y, x, bs = 'sos', k = 1e3) + # sos uses lat, long
+      ti(doy, y, x, bs = c('cc', 'sos'), d = c(1, 2), k = c(10, 250)) +
+      ti(year, y, x, bs = c('cr', 'sos'), d = c(1, 2), k = c(9, 250)) +
+      ti(doy, year, y, x, bs = c('cc', 'cr', 'sos'), d = c(1, 1, 2),
+         k = c(5, 5, 100)) +
+      s(elevation_m, bs = 'cr', k = 5),
+    family = gaussian(),
+    data = d,
+    method = 'fREML',
+    knots = list(doy = c(0.5, 366.5)),
+    discrete = TRUE,
+    samfrac = 0.01, # find initial guesses with a subset of the data
+    nthreads = future::availableCores(logical = FALSE) - 2,
+    control = gam.control(trace = TRUE))
+  
+  saveRDS(m_mu, paste0('models/global-models/hbam-mean-ndvi-sos-', DATE, '.rds'))
 }
 
-DATE <- paste0('-NO-MRF-NO-TI-THINNED-DATA-', format(Sys.Date(), '%Y-%m-%d'))
+100 * (1 - m_mu$deviance / m_mu$null.deviance)
+
+png(paste0('figures/global-models/mean-ndvi-terms-', DATE, '.png'),
+    width = 12, height = 9, units = 'in', bg = 'white', res = 300)
+plot(m_mu, pages = 1, too.far = 0.01, scheme = c(1, 1, 0, 5, 5, 5, 1),
+     phi = 90, theta = 0, n2 = 100)
+dev.off()
 
 # plot each smooth separately
-p_hbam_doy <- draw(m, rug = FALSE, discrete_colours = ,
-                   select = which(grepl('doy', smooths(m))))
+p_hbam_doy <- draw(m_mu, rug = FALSE,
+                   select = which(grepl('doy', smooths(m_mu))))
 ggsave(paste0('figures/hbam-mean-ndvi-doy-', DATE, '.png'),
        plot = p_hbam_doy, width = 8, height = 6, units = 'in', dpi = 300,
        bg = 'white')
 
-p_hbam_y <- draw(m, rug = FALSE,
-                 select = which(grepl('year', smooths(m))))
+p_hbam_y <- draw(m_mu, rug = FALSE,
+                 select = which(grepl('year', smooths(m_mu))))
 ggsave(paste0('figures/hbam-mean-ndvi-year-', DATE, '.png'),
        plot = p_hbam_y, width = 8, height = 6, units = 'in', dpi = 300,
        bg = 'white')
 
-p_hbam_elev <- draw(m, rug = FALSE,
-                    select = which(smooths(m) == 's(elevation_m)'))
+p_hbam_elev <- draw(m_mu, rug = FALSE,
+                    select = which(smooths(m_mu) == 's(elevation_m)'))
 ggsave(paste0('figures/hbam-mean-ndvi-elev_m-', DATE, '.png'),
        plot = p_hbam_elev, width = 8, height = 6, units = 'in', dpi = 300,
        bg = 'white')
 
-# p_hbam <- draw(m, rug = FALSE, select = - which(smooths(m) == 's(poly_id)'))
+# p_hbam <- draw(m_mu, rug = FALSE, select = - which(smooths(m_mu) == 's(poly_id)'))
 # 
 # ggsave(paste0('figures/hbam-mean-ndvi-', DATE, '.png'), plot = p_hbam,
 #        width = 8, height = 6, units = 'in', dpi = 600, bg = 'white')
 
 # find pixel-level variance ----
-# add fitted values and residuals to the dataset
-d <- m$model
-d$mu_hat <- fitted(m)
-d$e <- residuals(m)
+# add fitted values and residuals to the dataset (link function = identity)
+nrow(d) == length(fitted(m_mu))
+d0 <- d
+d <- m_mu$model
+d$mu_hat <- fitted(m_mu)
+d$e <- residuals(m_mu)
 
 # check pixel-level mean residuals
 d %>%
-  slice_sample(prop = 0.1) %>%
+  rename(long = x, lat = y) %>%
   group_by(long, lat) %>%
   summarize(mean_e = mean(e, na.rm = TRUE), .groups = 'drop') %>%
   pull(mean_e) %>%
-  hist()
+  hist(breaks = 100)
 
-#' **IF REMOVING PIXEL-LEVEL MEAN RESIDUAL**
-if(FALSE) {
-  d$e <- d$e - d$mean_e
-  d$mu_hat <- d$mu_hat + d$mean_e
-}
+d %>%
+  rename(long = x, lat = y) %>%
+  group_by(long, lat) %>%
+  summarize(median_e = median(e, na.rm = TRUE), .groups = 'drop') %>%
+  pull(median_e) %>%
+  hist(breaks = 100)
 
 # calculate pixel-level squared residual (mean e^2 is the variance)
 d$e_2 <- d$e^2
 
-saveRDS(d, paste0('data/hbam-var-ndvi-data', DATE, '.rds'))
+saveRDS(d, paste0('data/hbam-var-ndvi-data-', DATE, '.rds'))
--- analysis/012-var-ndvi-hgam.R
+++ analysis/012-var-ndvi-hgam.R
@@ -4,68 +4,164 @@ library('mgcv')    # for Genralized Additive Models
 library('sf')      # for simple fleature objects
 library('terra')   # for rasters
 library('gratia')  # for plotting GAMs
+library('ggplot2') # for fancy plots
 source('analysis/figures/000-default-ggplot-theme.R')
 
-ecoregions <- st_read('data/ecoregions/ecoregions-polygons.shp')
+# ecoregions <- st_read('data/ecoregions/ecoregions-polygons.shp')
 
 # import the data for the model
-d <- readRDS('data/hbam-var-ndvi-data.rds')
-nbs <- readRDS('data/ecoregions/poly-nbs-global.rds')
+d <- readRDS('data/hbam-var-ndvi-data-mod-5-no-res-2025-04-21-THINNED-50.rds')
+
+hist(d$mu_hat)
+quantile(d$mu_hat, 0.001)
+# d <- d %>%
+#   mutate(mu_hat = if_else(mu_hat < quantile(mu_hat, 0.001),
+#                           quantile(mu_hat, 0.001), mu_hat),
+#          mu_hat = if_else(mu_hat > 1, 1, mu_hat))
+# nbs <- readRDS('data/ecoregions/poly-nbs-global.rds')
 
 if(length(list.files('models/global-test', 'hbam-var-ndvi-*')) > 1) {
   DATE <- '2025-04-XX'
-  m <- readRDS(paste0('models/global-test/hbam-var-ndvi-', DATE, '.rds'))
+  m_s2 <- readRDS(paste0('models/global-test/hbam-var-ndvi-', DATE, '.rds'))
 } else {
-  m <- bam(
+  m_s2 <- bam(
     e_2 ~
-      biome + # to avoid intercept shrinkage
-      s(poly_id, bs = 'mrf', xt = list(nb = nbs)) +
-      s(doy, biome, bs = 'fs', xt = list(bs = 'cc'), k = 10) +
-      s(year, biome, bs = 'fs', xt = list(bs = 'cr'), k = 10) +
-      ti(doy, year, biome, bs = c('cc', 'cr', 're'), k = c(5, 5)) +
+      s(doy, bs = 'cc', k = 10) +
+      s(year, bs = 'cr', k = 9) +
+      s(y, x, bs = 'sos', k = 1e3) + # sos uses lat, long
+      ti(doy, y, x, bs = c('cc', 'sos'), d = c(1, 2), k = c(10, 250)) +
+      ti(year, y, x, bs = c('cr', 'sos'), d = c(1, 2), k = c(9, 250)) +
+      ti(doy, year, y, x, bs = c('cc', 'cr', 'sos'), d = c(1, 1, 2),
+         k = c(5, 5, 100)) +
       s(elevation_m, bs = 'cr', k = 5) +
-      s(mu_hat, bs = 'cr', k = 5),
+      s(mu_hat, k = 5),
     family = gaussian(),
     data = d,
     method = 'fREML',
     knots = list(doy = c(0.5, 366.5)),
-    drop.unused.levels = TRUE, # FALSE slows down the model substantially
     discrete = TRUE,
-    samfrac = 0.001, # find intial guesses with a subset of the data
+    samfrac = 0.01, # find initial guesses with a subset of the data
     nthreads = future::availableCores(logical = FALSE) - 2,
     control = gam.control(trace = TRUE))
   
-  saveRDS(m, paste0('models/global-test/hbam-var-ndvi-', DATE, '.rds'))
+  saveRDS(m_s2, paste0('models/global-models/hbam-mean-ndvi-sos-', DATE, '.rds'))
 }
 
+100 * (1 - m_s2$deviance / m_s2$null.deviance)
+
+png(paste0('figures/global-models/var-ndvi-terms-', DATE, '.png'),
+    width = 12, height = 9, units = 'in', bg = 'white', res = 300)
+plot(m_s2, pages = 1, too.far = 0.01, scheme = c(1, 1, 0, 5, 5, 5, 1, 1),
+     phi = 90, theta = 0, n2 = 100, scale = 0)
+dev.off()
+
 # plot each smooth separately
-p_hbam_doy <- draw(m, rug = FALSE,
-                   select = which(grepl('s(doy', smooths(m))))
+p_hbam_doy <- draw(m_s2, rug = FALSE,
+                   select = which(grepl('s(doy', smooths(m_s2))))
 ggsave(paste0('figures/hbam-var-ndvi-fe-doy-', DATE, '.png'),
        plot = p_hbam_doy, width = 24, height = 36, units = 'in', dpi = 300,
        bg = 'white')
 
-p_hbam_y <- draw(m, rug = FALSE,
-                 select = which(grepl('s(year', smooths(m))))
+p_hbam_y <- draw(m_s2, rug = FALSE,
+                 select = which(grepl('s(year', smooths(m_s2))))
 ggsave(paste0('figures/hbam-var-ndvi-fe-year-', DATE, '.png'),
        plot = p_hbam_y, width = 24, height = 36, units = 'in', dpi = 300,
        bg = 'white')
 
-p_hbam_elev <- draw(m, rug = FALSE,
-                    select = which(smooths(m) == 's(elev_m)'))
+p_hbam_elev <- draw(m_s2, rug = FALSE,
+                    select = which(smooths(m_s2) == 's(elev_m)'))
 ggsave(paste0('figures/hbam-var-ndvi-fe-elev_m-', DATE, '.png'),
        plot = p_hbam_elev, width = 4, height = 6, units = 'in', dpi = 300,
        bg = 'white')
 
-p_hbam <- draw(m, rug = FALSE, - which(smooths(m) == 's(poly_id)'))
+p_hbam <- draw(m_s2, rug = FALSE, - which(smooths(m_s2) == 's(poly_id)'))
 
 ggsave(paste0('figures/hbam-var-ndvi-', DATE, '.png'), plot = p_hbam,
        width = 8, height = 12, units = 'in', dpi = 600, bg = 'white')
 
 # hex plot of mean and variance
-ggplot() +
-  geom_hex(aes(mu_hat, fitted(m))) +
-  labs(x = 'Estimated mean NDVI', y = 'Estimated variance in NDVI')
+d %>%
+  mutate(s2_hat = fitted(m_s2)) %>%
+  filter(s2_hat < 0.25) %>% #' *remove*
+  ggplot() +
+  geom_hex(aes(mu_hat, s2_hat, fill = log10(after_stat(count))),
+           color = 'black', bins = 100, linewidth = 0.1, na.rm = TRUE) +
+  labs(x = 'Estimated mean NDVI', y = 'DENVar') +
+  scale_fill_iridescent(
+    name = expression(paste(bold('Count (log'), bold(''['10']),
+                            bold(' scale)'))), range = c(0, 1),
+    reverse = FALSE, breaks = seq(0, 6, by = 1),
+    labels = \(.x) 10^.x) +
+  theme(legend.position = 'top', legend.key.width = unit(0.5, 'in'))
 
-ggsave('figures/hbam-var-mean-ndvi-hex.png',
-       width = 8, height = 12, units = 'in', dpi = 600, bg = 'white')
+ggsave(paste0('figures/global-models/hbam-var-mean-ndvi-hex-', DATE, '.png'),
+       width = 8, height = 6, units = 'in', dpi = 600, bg = 'white')
+
+# make rasters of space
+dem <- rast('data/elev-raster.tif')
+ecoregions <- read_sf('data/ecoregions/ecoregions-polygons.shp') %>%
+  # drop polygons not in data
+  filter(poly_id %in% names(readRDS('data/ecoregions/poly-nbs-global.rds'))) %>%
+  vect() %>% # convert to spatVect
+  project(crs(dem))
+
+preds <- dem %>%
+  crop(ecoregions) %>%
+  mask(ecoregions) %>%
+  as.data.frame(dem, xy = TRUE) %>%
+  rename(elevation_m = 3) %>%
+  mutate(doy = 0, year = 0) %>%
+  mutate(mu_hat = predict.bam(m_mu, newdata = ., discrete = TRUE,
+                          terms = c('(Intercept)', 's(y,x)', 's(elevation_m)'))) %>%
+  mutate(s2_hat = predict(m_s2, newdata = ., discrete = TRUE,
+                          terms = c('(Intercept)', 's(y,x)', 's(elevation_m)')))
+head(preds)
+
+r_mu <- preds %>%
+  select(x, y, mu_hat) %>%
+  rast(crs = crs(dem))
+
+r_s2 <- preds %>%
+  select(x, y, s2_hat) %>%
+  rast(crs = crs(dem))
+
+plot(r_mu)
+plot(r_s2)
+plot(values(r_s2) ~ values(r_mu))
+
+writeRaster(r_mu, paste0('data/output/mean-ndvi-raster-', DATE, '.tif'), overwrite = TRUE)
+writeRaster(r_s2, paste0('data/output/var-ndvi-raster-', DATE, '.tif'), overwrite = TRUE)
+
+rast('H:/GitHub/ndvi-stochasticity/data/output/mean-ndvi-raster-mod-5-no-res-2025-04-21-THINNED-50.tif') %>%
+  as.data.frame(xy = TRUE) %>%
+  mutate(mu_hat = case_when(
+    mu_hat < quantile(mu_hat, 0.0001) ~ quantile(mu_hat, 0.0001),
+    mu_hat > quantile(mu_hat, 0.9999) ~ quantile(mu_hat, 0.9999),
+    .default = mu_hat)) %>%
+  ggplot() +
+  geom_raster(aes(x, y, fill = mu_hat)) +
+  geom_sf(data = st_as_sf(ecoregions), color = 'grey30', linewidth = 0.1,
+          fill = 'transparent', inherit.aes = FALSE) +
+  scale_x_continuous(expand = c(0, 0)) +
+  scale_fill_viridis_c('Mean NDVI') +
+  labs(x = NULL, y = NULL) +
+  theme(legend.position = 'top', legend.key.width = unit(0.5, 'in'))
+ggsave('H:/GitHub/ndvi-stochasticity/figures/global-models/output/mean-ndvi-raster-mod-5-no-res-2025-04-21-THINNED-50.png',
+       width = 8, height = 4, units = 'in', bg = 'white', dpi = 300)
+
+rast('H:/GitHub/ndvi-stochasticity/data/output/var-ndvi-raster-mod-5-no-res-2025-04-21-THINNED-50.tif') %>%
+  as.data.frame(xy = TRUE) %>%
+  mutate(s2_hat = case_when(
+    s2_hat < quantile(s2_hat, 0.0001) ~ quantile(s2_hat, 0.0001),
+    s2_hat > 0.04 ~ 0.04,
+    .default = s2_hat)) %>%
+  ggplot() +
+  geom_raster(aes(x, y, fill = s2_hat)) +
+  geom_sf(data = st_as_sf(ecoregions), color = 'grey30', linewidth = 0.1,
+          fill = 'transparent', inherit.aes = FALSE) +
+  scale_x_continuous(expand = c(0, 0)) +
+  scale_fill_viridis_c('DENVar, capped at 0.04', option = 'A') +
+  labs(x = NULL, y = NULL) +
+  theme(legend.position = 'top', legend.key.width = unit(0.5, 'in'))
+ggsave('H:/GitHub/ndvi-stochasticity/figures/global-models/output/var-ndvi-raster-mod-5-no-res-2025-04-21-THINNED-50.png',
+       width = 8, height = 4, units = 'in', bg = 'white', dpi = 300)
