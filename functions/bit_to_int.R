c9f76b37abe065b3d70c9b65afcf205babf72b74
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2025-05-30 11:27:31 -0700

    fixed singulars; added safety check on length

--- functions/bit_to_int.R
+++ functions/bit_to_int.R
@@ -1,12 +1,17 @@
 # a function to convert bits to integers/numeric
 #' *NOTE: this function uses R-type (i.e., reversed) bits!*
-bit_to_int <- function(bits) {
-  if(class(bits) == 'raw') {
-    bits <- as.numeric(bits)
-  } else if(! class(bits) %in% c('numeric', 'integer')) {
-    stop('`bits` must be of class "raw", "numeric" or "integer".\n')
+bit_to_int <- function(bit) {
+  if(class(bit) == 'raw') {
+    bit <- as.numeric(bit)
+  } else if(! class(bit) %in% c('numeric', 'integer')) {
+    stop('`bit` must be of class "raw", "numeric" or "integer".\n')
   }
-  as.numeric(t(2^(0:14)) %*% bits[1:15] - 2^15 * bits[16])
+  
+  if(length(bit) != 16) {
+    stop('This function can only deal with 16-bit signed shorts.')
+  }
+  
+  as.numeric(t(2^(0:14)) %*% bit[1:15] - 2^15 * bit[16])
 }
 
 if(FALSE) { # some tests
