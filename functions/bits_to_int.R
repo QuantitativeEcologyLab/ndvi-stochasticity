ddd839841fd5420b491ab7cfad9a51d020527e24
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2025-05-30 06:15:15 -0700

    made function to convert bits to integer

---
+++ functions/bits_to_int.R
@@ -0,0 +1,8 @@
+bits_to_int <- function(bits) {
+  if(class(bits) == 'raw') {
+    bits <- as.numeric(bits)
+  } else if(! class(bits) %in% c('numeric', 'integer')) {
+    stop('`bits` must be of class "raw", "numeric" or "integer".\n')
+  }
+  as.numeric(t(2^(0:14)) %*% bits[1:15] - 2^15 * bits[16])
+}
