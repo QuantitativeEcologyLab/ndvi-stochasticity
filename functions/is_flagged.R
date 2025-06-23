a46c5a7ff025dd322224b6fe7b9f7c68023307e2
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2025-06-23 10:01:26 -0700

    fixed comments

--- functions/is_flagged.R
+++ functions/is_flagged.R
@@ -1,12 +1,13 @@
 #' to drop values flagged with quality flag assurance `.qa`. Note that the
 #' flag positions are counted starting from 0 for consistency. For example,
 #' if `flag = 2` and we want to exclude values (10, 11) and keep (00, 11),
-#' we can use `QA %% (2 * 2) < 2`:
-#' 00 keep; return TRUE  to keep
-#' 01 keep; return TRUE  to keep
-#' 10 drop; return FALSE to drop
-#' 10 drop; return FALSE to drop
+#' we can use `is_flagged() == (QA %% (2 * 2) >= 2)`:
+#' 00 keep; return TRUE  to drop
+#' 01 keep; return TRUE  to drop
+#' 10 drop; return FALSE to keep
+#' 10 drop; return FALSE to keep
 #' if you want to do the opposite, just negate the output with `!`
+#' `QA` can be a single value or a SpatRaster
 source('functions/bit_to_int.R')
 
 is_flagged <- function(QA, flag_position) {
@@ -16,6 +17,7 @@ is_flagged <- function(QA, flag_position) {
   if(length(flag_position) > 1) {
     stop('`flag_position` must be of length 1.\n')
   }
+  
   bit <- numeric(16)
   bit[flag_position + 1] <- 1
   bit_value <- bit_to_int(bit = bit) 
