0a46a30fe69c188f5c478fcb3f77129060a6bdeb
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2025-08-12 11:16:45 -0700

    fixed comments, added gaussian sampling issues

--- analysis/misc/visualizing-mean-and-varinance-in-ndvi.R
+++ analysis/misc/visualizing-mean-and-varinance-in-ndvi.R
@@ -9,13 +9,22 @@ rbeta2 <- function(mu, s2) {
        ylab = '', xlab = 'NDVI', xlim = c(0, 1))
 }
 
-layout(matrix(1:4, ncol = 2))
-rbeta2(mu = 0.5, s2 = 0.01)
-rbeta2(mu = 0.5, s2 = 0.25) # note: scale is 0.25 / (0.5 * 0.5) = 1
-rbeta2(mu = 0.9, s2 = 0.02)
-rbeta2(mu = 0.9, s2 = 0.089999) # note: scale is 0.089999 / (0.9 * 0.1) = 0.9999889 ~= 1
+layout(matrix(1:4, ncol = 2, byrow = TRUE))
 
-rbeta2(mu = 0.5, s2 = 0.002)
-rbeta2(mu = 0.5, s2 = 0.02) # note: scale is 0.25 / (0.5 * 0.5) = 1
+# the max value of the variance depends on the mean
+rbeta2(mu = 0.5, s2 = 0.01)
+rbeta2(mu = 0.5, s2 = 0.25) # scale is 0.25 / (0.5 * 0.5) = 1
 rbeta2(mu = 0.9, s2 = 0.02)
-rbeta2(mu = 0.1, s2 = 0.02) # note: scale is 0.089999 / (0.9 * 0.1) = 0.9999889 ~= 1
+rbeta2(mu = 0.9, s2 = 0.089999) # scale is 0.089999 / (0.9 * 0.1) ~= 1
+
+# s2 = 0.01 is approximately the upper limit for reasonable distributions
+rbeta2(mu = 0.5, s2 = 0.001)
+rbeta2(mu = 0.5, s2 = 0.01)
+rbeta2(mu = 0.5, s2 = 0.02)
+rbeta2(mu = 0.5, s2 = 0.04)
+
+# s2 > 0.02 easily gives gaussian samples with values < 0 or > 1
+hist(rnorm(1e3, mean = 0.5, sd = sqrt(0.001))); abline(v = 0:1, col = 'red')
+hist(rnorm(1e3, mean = 0.5, sd = sqrt(0.01))); abline(v = 0:1, col = 'red')
+hist(rnorm(1e3, mean = 0.5, sd = sqrt(0.02))); abline(v = 0:1, col = 'red')
+hist(rnorm(1e3, mean = 0.5, sd = sqrt(0.04))); abline(v = 0:1, col = 'red')
