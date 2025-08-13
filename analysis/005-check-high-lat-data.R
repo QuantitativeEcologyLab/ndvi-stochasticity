bc4009e81ed39117aea88d2168856d46f779ac44
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2025-08-13 10:38:34 -0700

    created figure of monthly latitudinal ECDF

--- analysis/005-check-high-lat-data.R
+++ analysis/005-check-high-lat-data.R
@@ -179,7 +179,148 @@ ggsave('figures/global-diagnostics/monthly-max-values-above-0.2-hist.png',
        p_max_hist, width = 15, height = 10, units = 'in', dpi = 300,
        bg = 'white')
 
+# there are some bands of extremely high NDVI for high latitudes
+NCORES <- min(availableCores() - 4, 60)
+plan(multisession, workers = NCORES)
 
+#' *using the full dataset results in the same, repeated error:*
+#' Error in `mutate()`:
+#' ℹ In argument: `month_data = future_map(...)`.
+#' Caused by error:
+#' ℹ In index: 1.
+#' Caused by error in `vec_rbind()`:
+#' ! Negative `n` in `compact_rep()`.
+#' ℹ In file 'utils.c' at line 897.
+#' ℹ This is an internal error that was detected in the vctrs package.
+if(file.exists('data/avhrr-viirs-ndvi/global-diagnostics/monthly-ecdf-summary-without-ecdf-10-percent-subset.rds')) {
+  d_ecdf <- readRDS('data/avhrr-viirs-ndvi/global-diagnostics/monthly-ecdf-summary-without-ecdf-10-percent-subset.rds')
 } else {
+  d_ecdf <-
+    tibble(
+      fn = c(
+        # AVHRR files
+        list.files(
+          path = 'data/avhrr-viirs-ndvi/raster-files/AVHRR-Land_v006',
+          recursive = TRUE, full.names = TRUE),
+        # VIIRS files
+        list.files(
+          path = 'data/avhrr-viirs-ndvi/raster-files/VIIRS-Land-v001',
+          recursive = TRUE, full.names = TRUE)),
+      date =
+        if_else(grepl('AVHRR', fn),
+                gsub('.*\\N.._AVH13C1.A', '', fn) %>%
+                  gsub('\\..*', '', .) %>%
+                  as.Date(format = '%Y%j'),
+                substr(fn,
+                       nchar(fn) - nchar('YYYYMMDD_cYYYYMMDDHHMMSS.nc') + 1,
+                       nchar(fn) - nchar('_cYYYYMMDDHHMMSS.nc')) %>%
+                  as.Date(format = '%Y%m%d')),
+      year = year(date),
+      month = factor(month.name[month(date)], levels = month.name)) %>%
+    slice_sample(prop = 0.1) %>%
+    nest(month_data = ! month) %>%
+    mutate(month_data = future_map(month_data, \(.d) {
+      map(.d$fn, \(.fn) {
+        .r <- rast(.fn) %>%
+          # crop and mask to land area only
+          crop(st_transform(ecoregions, crs(.)), mask = TRUE)
+        
+        # drop cloudy pixels
+        .r <- ifel(is_flagged(.r$QA, 1), NA, .r$NDVI)
+        
+        return(as.data.frame(.r, xy = TRUE))
+      }) %>%
+        bind_rows() %>%
+        summarize(cdf = list(ecdf(NDVI)), .by = y) %>%
+        mutate(probs = map(cdf, \(.fun) {
+          tibble(ndvi = seq(-0.1, 1, by = 0.01),
+                 p = .fun(ndvi))
+        })) %>%
+        return()
+    }, .progress = TRUE, .options = furrr_options(seed = NULL))) %>%
+    unnest(month_data) %>%
+    unnest(probs)
+  
+  saveRDS(d_ecdf, 'data/avhrr-viirs-ndvi/global-diagnostics/monthly-ecdf-summary-10-percent-subset.rds')
+  
+  # saving a version without ECDF to be able to push it to GitHub
+  d_ecdf %>%
+    select(! cdf) %>%
+    saveRDS('data/avhrr-viirs-ndvi/global-diagnostics/monthly-ecdf-summary-without-ecdf-10-percent-subset.rds')
 }
 
+# find NDVI values closest to the 99th percentile for each latitude
+q_99 <- d_ecdf %>%
+  slice(which.min(abs(p - 0.99)), .by = c(month, y)) %>%
+  # find NDVI cutoffs after dropping decimals in latitude
+  mutate(month = factor(month.name[month], levels = month.name),
+         y_2 = floor(y)) %>%
+  mutate(ndvi_2 = min(ndvi), .by = c(y_2, month)) %>%
+  #' arrange by latitude to make sure `geom_path()` looks right
+  arrange(y)
+
+ggplot(q_99) +
+  facet_wrap(~ month) +
+  geom_path(aes(ndvi_2, y)) +
+  scale_x_continuous('NDVI', expand = c(0, 0)) +
+  theme(panel.grid = element_blank(), legend.position = 'top',
+        text = element_text(face = 'bold'))
+
+cutoffs <- expand_grid(month = unique(q_99$month),
+                       y = unique(q_99$y),
+                       ndvi = c(0, 1)) %>%
+  filter((month == 'January' & ! y < 60) |
+           (month == 'February' & ! y < 65) |
+           (month == 'March' & ! y < 70) |
+           (month == 'September' & ! y < 80) |
+           (month == 'October' & ! y < 69) |
+           (month == 'November' & ! y < 62) |
+           (month == 'December' & ! y < 59)) %>%
+  summarize(ymin = min(y), ymax = max(y), .by = c(month, ndvi))
+
+# filter months based on q_99 (black lines or april ref line)
+# bright bands simply won't have much data, but the bands are thin anyway
+p_cutoffs <-
+  ggplot() +
+  facet_wrap(~ month) +
+  geom_raster(aes(ndvi, y, fill = p), d_ecdf) +
+  geom_rect(aes(xmin = 0, xmax = Inf, ymin = ymin, ymax = Inf), cutoffs,
+            fill = '#8B102080', color = '#8B0000', linetype = 'dashed') +
+  geom_path(aes(ndvi, y), q_99, color = 'white', linewidth = 0.1) +
+  geom_path(aes(ndvi_2, y), q_99, color = 'white', linewidth = 0.3) +
+  scale_fill_tokyo(name = 'ECDF(NDVI)', reverse = TRUE) +
+  scale_x_continuous('NDVI', expand = c(0, 0)) +
+  scale_y_continuous('Latitude', expand = c(0, 0), limits = c(-90, 90),
+                     breaks = seq(-90, 90, by = 30)) +
+  theme(panel.grid = element_blank(), legend.position = 'top',
+        text = element_text(face = 'bold'),
+        panel.background = element_rect(fill = 'grey'),
+        plot.margin = unit(c(5.5, 11, 5.5, 5.5), 'points'))
+
+colorblindr::cvd_grid(p_cutoffs, severity = 1)
+
+ggsave('figures/global-diagnostics/monthly-latitudinal-ecdf.png', p_cutoffs,
+       width = 12, height = 10, units = 'in', dpi = 300, bg = 'white')
+
+# the change in cutoff throughout the year can be estimated by a function
+# with very little uncertainty, but using these smooth changes risks
+# leaving some extreme values in the data. it may be worth doing the check
+# at intervals finer than a month (e.g., 7 or 14 days), but we can probably
+# make the change in future versions, if necessary.
+if(FALSE) {
+  doy_cutoffs <- tibble(
+    date = c('2025-01-15', '2025-02-15', '2025-03-15', '2025-04-15',
+             '2025-05-15', '2025-06-15', '2025-07-15', '2025-08-15',
+             '2025-09-15', '2025-10-15', '2025-11-15', '2025-12-15') %>%
+      as.Date(),
+    doy = yday(date),
+    month = month(date),
+    y = c(60, 65, 70, rep(NA, 5), 80, 69, 62, 59))
+  
+  m_cutoffs <- gam(y ~ s(doy, bs = 'cc', k = 7), data = doy_cutoffs,
+                   method = 'REML', knots = list(doy = c(0.5, 365.5)))
+  plot.gam(m_cutoffs, residuals = TRUE, scheme = 1, pch = 19,
+           xlim = c(0.5, 365.5), seWithMean = TRUE,
+           trans = \(x) x + coef(m_cutoffs)['(Intercept)'])
+  rm(m_cutoffs)
+}
