699727c7cdbcdbc5d6b9ae81e6d3a54e43fa8406
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2025-09-24 08:06:41 -0700

    moved white to [-0.1, 0]

--- analysis/figures/000-default-ggplot-theme.R
+++ analysis/figures/000-default-ggplot-theme.R
@@ -8,9 +8,9 @@ theme_set(theme_bw() +
                   text = element_text(face = 'bold')))
 
 # custom NDVI color palette
-ndvi_pal <- color('bukavu')(30) # wrong order
+ndvi_pal <- color('bukavu')(100) # wrong order
 if(.PLOT_PALETTES) plot_scheme(ndvi_pal)
-ndvi_pal <- ndvi_pal[c(seq(1, 13, by = 2), 30, 23, 21:16)]
+ndvi_pal <- ndvi_pal[c(1:30, seq(31, 50, by = 2), 100:51)]
 if(.PLOT_PALETTES) plot_scheme(ndvi_pal)
 create_ndvi_pal <- colorRampPalette(ndvi_pal)
 ndvi_pal <- create_ndvi_pal(1e3)
@@ -23,8 +23,12 @@ if(.PLOT_PALETTES) {
     geom_raster(aes(x, y, fill = z)) +
     scale_x_continuous(expand = c(0, 0)) +
     scale_y_continuous(expand = c(0, 0)) +
-    scale_fill_gradientn(colours = ndvi_pal, limits = c(-1, 1))
-  
+    scale_fill_gradientn(colours = ndvi_pal, limits = c(-1, 1),
+                         breaks = c(-1, -0.5, -0.1, 0, 0.5, 1),
+                         labels = paste) +
+    theme(legend.position = 'top', legend.key.width = rel(3))
+}
+if(.PLOT_PALETTES) {
   (expand.grid(x = 1:10,
                y = 1:10) %>%
      mutate(z = runif(100, min = -1, max = 1)) %>%
