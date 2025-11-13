71bad41b17b695ff0098ba98699e71edb4b97454
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2025-11-13 08:29:21 -0800

    fit neotropic+nearctic model; started preds
    started preds on 2025-11-07

--- analysis/010-global-models.R
+++ analysis/010-global-models.R
@@ -1,11 +1,13 @@
 # EME Linux: 2.2 TB RAM, 2 Intel Xeon Platinum 8462Y+ processors, 64 cores
 library('dplyr')     # for data wrangling
 library('purrr')     # for functional programing
+library('furrr')     # for parallelized functional programing
 library('lubridate') # for working with dates
 library('mgcv')      # for Genralized Additive Models
 library('gratia')    # for plotting GAMs
 library('qs')        # for saving R objects quicker than with saveRDS
 library('sf')        # for splitting predictions across groups
+library('terra')     # for working with rasters
 source('analysis/figures/000-default-ggplot-theme.R')
 
 NCORES <- future::availableCores(logical = FALSE) - 2
@@ -19,7 +21,7 @@ GROUPS <-
   unique() %>%
   tolower() %>%
   gsub(', ', '-', .)
-GROUPS <- GROUPS[4:1]
+GROUPS <- GROUPS[4]#:1]
 GROUPS
 DATE <- paste(Sys.Date()) # make a character to avoid bad conversions
 DATE
@@ -28,10 +30,10 @@ DATE
 # 3x aggregation maxes out RAM for neotropic-nearctic model
 # if 4x crashes, reduce k of tis to k_s/4
 # fitting times
-# neotropic-nearctic: 21-min setup, 22-min gam.setup, 1 h fit; ~1.7 h total
-# africa: 
-# palearctic: 
-# islands: 
+#' neotropic-nearctic: 2 days; 1.7 h w/o `ti(year, doy, x, y)` 
+#' africa: 
+#' palearctic: 
+#' islands: 
 models <-
   tibble(group = GROUPS,
          data = map(group, function(.g) {
@@ -40,106 +42,139 @@ models <-
              qread(nthreads = NCORES)
          }),
          m_mu = map2(group, data, function(.g, .d) {
-           gc() # clean up before starting
-           
-           k_s <-
-             case_when(.g == 'africa' ~ 2000,
-                       .g == 'indo-malay-oceania-australasia' ~ 900,
-                       .g == 'neotropic-nearctic' ~ 1300,
-                       .g == 'palearctic' ~ 2000)
-           
-           .m_mu <- bam(
-             ndvi_aggr ~
-               # marginal smooths
-               s(prop_water, bs = 'cr', k = 5) + # water biases towards < 0
-               s(y, x, bs = 'sos', k = k_s) + # smooth of space
-               s(year, bs = 'cr', k = 20) + # year effect
-               s(doy, bs = 'cc', k = 10) + # seasonal/day of year effect
-               s(elev_m, bs = 'cr', k = 10) + # elevation effect
-               # tensor product interaction smooths
-               # seasonal trends vary over the years
-               ti(year, doy, bs = c('cr', 'cc'), k = c(10, 10)) +
-               # yearly trends vary spatially
-               ti(year, y, x, bs = c('cr', 'sos'), d = c(1, 2), k = c(10, k_s / 4)) +
-               # seasonal trends vary spatially
-               ti(doy, y, x, bs = c('cc', 'sos'), d = c(1, 2), k = c(10, k_s / 4)) +
-               # change in seasonal trends over the years varies spatially
-               # (e.g., seasonal trends in the arctic change differently
-               # from those at the equator)
-               ti(year, doy, y, x, bs = c('cr', 'cc', 'sos'), d = c(1, 1, 2),
-                  k = c(10, 10, k_s / 4)),
-             family = gaussian(),
-             data = .d,
-             method = 'fREML',
-             knots = list(doy = c(0.5, 366.5)),
-             discrete = TRUE,
-             nthreads = NCORES,
-             control = gam.control(trace = TRUE),
-             samfrac = 0.01) # initial guess using only 1% of data (default = 100%)
-           
-           qsave(.m_mu, paste0('models/global-models/bam-mean-ndvi-', .g, '-',
-                               DATE, '.qs'), nthreads = NCORES)
+           if(length(list.files('H:/GitHub/ndvi-stochasticity/models/global-models',
+                                paste0('bam-mean-ndvi-', .g))) > 0) {
+             .m_mu <-
+               qread(list.files('models/global-models',
+                                paste0('bam-mean-ndvi-', .g),
+                                full.names = TRUE),
+                     nthreads = NCORES)
+           } else {
+             gc() # clean up before starting
+             
+             k_s <-
+               case_when(.g == 'africa' ~ 2000,
+                         .g == 'indo-malay-oceania-australasia' ~ 900,
+                         .g == 'neotropic-nearctic' ~ 1300,
+                         .g == 'palearctic' ~ 2000)
+             
+             .m_mu <- bam(
+               ndvi_aggr ~
+                 # marginal smooths
+                 s(prop_water, bs = 'cr', k = 5) + # water biases towards < 0
+                 s(y, x, bs = 'sos', k = k_s) + # smooth of space
+                 s(year, bs = 'cr', k = 20) + # year effect
+                 s(doy, bs = 'cc', k = 10) + # seasonal/day of year effect
+                 s(elev_m, bs = 'cr', k = 10) + # elevation effect
+                 # tensor product interaction smooths
+                 # seasonal trends vary over the years
+                 ti(year, doy, bs = c('cr', 'cc'), k = c(10, 10)) +
+                 # yearly trends vary spatially
+                 ti(year, y, x, bs = c('cr', 'sos'), d = c(1, 2), k = c(10, k_s / 4)) +
+                 # seasonal trends vary spatially
+                 ti(doy, y, x, bs = c('cc', 'sos'), d = c(1, 2), k = c(10, k_s / 4)) +
+                 # change in seasonal trends over the years varies spatially
+                 # (e.g., seasonal trends in the arctic change differently
+                 # from those at the equator)
+                 ti(year, doy, y, x, bs = c('cr', 'cc', 'sos'), d = c(1, 1, 2),
+                    k = c(10, 10, k_s / 4)),
+               family = gaussian(),
+               data = .d,
+               method = 'fREML',
+               knots = list(doy = c(0.5, 366.5)),
+               discrete = TRUE,
+               nthreads = NCORES,
+               control = gam.control(trace = TRUE),
+               samfrac = 0.01) # initial guess using only 1% of data (default = 100%)
+             
+             qsave(.m_mu, paste0('models/global-models/bam-mean-ndvi-', .g, '-',
+                                 DATE, '.qs'), nthreads = NCORES)
+           }
            return(.m_mu)
          }, .progress = TRUE))
 
-#' *STILL NEED TO CREATE RASTERS OF CLEANED AND AGGREGATED DATA*
 # find pixel-level variance ----
-raw_file_names <-
-  list.files('data/avhrr-viirs-ndvi/raster-files/cleaned-and-aggregated',
-             full.names = TRUE)
+m_mu_nn <- models$m_mu[[1]]
+d_nn <- models$data[[1]]
+d_nn$mu_hat <- predict.bam(m_mu_nn, discrete = FALSE, block.size = 1e6,
+                           newdata.guaranteed = TRUE)
+d_nn <- mutate(d_nn, e2 = (ndvi_aggr - mu_hat)^2)
 
-group_shp <- read_sf('data/ecoregions/groups-polygons.shp', quiet = TRUE) %>%
-  select(! area_km2) %>%
-  filter(group != 'Antarctic')
+paste0('data/output/neotropic-nearctic/',
+       c('aggregated-unmodeled-ndvi',
+         'estimated-mean-ndvi-fitted-only',
+         'squared-ndvi-residuals')) %>%
+  map_chr(function(.dir) {
+    dir.create(.dir)
+    return(paste(.dir, 'created.'))
+  })
 
-#' *CHECK BEFORE RUNNING*
-future_map(raw_file_names, function(.fn) {
-  preds <- rast(.fn) %>%
-    as.data.frame(xy = TRUE, na.rm = TRUE) %>%
-    # add model covariates
-    mutate(.,
-           date =
-             gsub('*.unmodeled-ndvi-', '', .fn) %>%
-             gsub('-t-4-s-4.tif', '', .) %>%
-             as_date(),
-           year = year(date),
-           doy = yday(date),
-           elev_m = extract(r_elev, select(., x, y))[, 2],
-           prop_water = 0,
-           group = select(., x, y) %>%
-             st_as_sf(coords = c('x', 'y')) %>%
-             st_set_crs('EPSG:4326') %>%
-             st_transform(st_crs(group_shp)) %>%
-             mutate(.,
-                    group = st_intersects(., group_shp),
-                    group = as.numeric(group),
-                    group = group_shp$group[group]) %>%
-             pull(group)) %>%
-    nest(group_data = ! group) %>%
-    # add predictions and squared residuals
-    mutate(group_data = map2(group, group_data, function(..g, ..data) {
-      ..model <- models$model[[which(models$group == ..g)]]
-      
-      
-      mutate(..data,
-             mu_hat = predict(m_mu, newdata = d, type = 'response',
-                              discrete = FALSE),
-             e = ndvi_aggr - mu_hat,
-             e2 = (e)^2)
-    })) %>%
-    unnest(group_data)
-  
-  preds %>%
-    select(x, y, mu_hat, e, e2) %>%
-    rast() %>%
-    writeRaster(filename = c(
-      gsub('cleaned-and-aggregated/unmodeled-ndvi',
-           'estimated-means/mu-hat', .fn),
-      gsub('cleaned-and-aggregated/unmodeled-ndvi',
-           'residuals/residuals', .fn),
-      gsub('cleaned-and-aggregated/unmodeled-ndvi',
-           'squared-residuals/squared-residuals', .fn)))
-})
+plan('multisession', workers = NCORES)
+
+d_nn %>%
+  select(central_date, x, y, ndvi_aggr, mu_hat, e2) %>%
+  nest(r = ! central_date) %>%
+  future_map2(as.character(central_date), r, function(.date, .r) {
+    .r %>%
+      rast() %>%
+      terra::`crs<-`('EPSG:4326')%>%
+      writeRaster(
+        paste0('data/outputs/neotropic-nearctic/',
+               c('aggregated-unmodeled-ndvi/observed-ndvi-',
+                 'estimated-mean-ndvi-fitted-only/fitted-',
+                 'squared-ndvi-residuals/e2-'),
+               .date, '.tif'))
+    return(paste('Exported', .date))
+  }, .options = furrr_options(seed = NULL), .progress = TRUE)
+
+#' *DELETE? CHECK BEFORE RUNNING*
+# future_map(raw_file_names, function(.fn) {
+#   preds <- rast(.fn) %>%
+#     as.data.frame(xy = TRUE, na.rm = TRUE) %>%
+#     # add model covariates
+#     mutate(.,
+#            date =
+#              gsub('*.unmodeled-ndvi-', '', .fn) %>%
+#              gsub('-t-4-s-4.tif', '', .) %>%
+#              as_date(),
+#            year = year(date),
+#            doy = yday(date),
+#            elev_m = extract(r_elev, select(., x, y))[, 2],
+#            prop_water = 0,
+#            group = select(., x, y) %>%
+#              st_as_sf(coords = c('x', 'y')) %>%
+#              st_set_crs('EPSG:4326') %>%
+#              st_transform(st_crs(group_shp)) %>%
+#              mutate(.,
+#                     group = st_intersects(., group_shp),
+#                     group = as.numeric(group),
+#                     group = group_shp$group[group]) %>%
+#              pull(group)) %>%
+#     nest(group_data = ! group) %>%
+#     # add predictions and squared residuals
+#     mutate(group_data = map2(group, group_data, function(..g, ..data) {
+#       ..model <- models$model[[which(models$group == ..g)]]
+#       
+#       
+#       mutate(..data,
+#              mu_hat = predict(m_mu, newdata = d, type = 'response',
+#                               discrete = FALSE),
+#              e = ndvi_aggr - mu_hat,
+#              e2 = (e)^2)
+#     })) %>%
+#     unnest(group_data)
+#   
+#   preds %>%
+#     select(x, y, mu_hat, e, e2) %>%
+#     rast() %>%
+#     writeRaster(filename = c(
+#       gsub('cleaned-and-aggregated/unmodeled-ndvi',
+#            'estimated-means/mu-hat', .fn),
+#       gsub('cleaned-and-aggregated/unmodeled-ndvi',
+#            'residuals/residuals', .fn),
+#       gsub('cleaned-and-aggregated/unmodeled-ndvi',
+#            'squared-residuals/squared-residuals', .fn)))
+# })
 
 #' # check pixel-level residuals *EDIT OR REMOVE*
 # if(FALSE) {
