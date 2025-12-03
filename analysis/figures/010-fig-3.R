1f350be9203cba2ffb6bc7016013981b20430db6
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2025-12-03 12:18:07 -0600

    added annual precip

--- analysis/figures/010-fig-3.R
+++ analysis/figures/010-fig-3.R
@@ -7,14 +7,20 @@ library('cowplot') # for ggplot plots in grids
 source('analysis/figures/000-default-ggplot-theme.R')
 source('functions/get_legend.R')
 
-r_s2 <- rast('data/output/var-ndvi-raster-mod-5-no-res-2025-04-21-THINNED-50.tif')
+r_s2 <- rast('data/output/var-ndvi-raster-mod-5-no-res-2025-04-21-THINNED-50.tif') %>%
+  ifel(. > 0.05, 0.05, .)
 ecoregions <- read_sf('data/ecoregions/groups-polygons.shp') %>%
   vect() %>% # convert to spatVect
   project(crs(r_s2))
 r_s2 <- crop(r_s2, ecoregions, mask = TRUE)
 r_mu <- rast('data/output/mean-ndvi-raster-mod-5-no-res-2025-04-21-THINNED-50.tif')
-r_dhi <- rast('data/other-rasters/dhi-data/dhi_ndvi_2015.tif')
-r_hfi <- rast('H:/GitHub/ndvi-stochasticity/data/other-rasters/hfp_2021_100m_v1-2_cog.tif')
+r_dhi <- rast('data/other-rasters/dhi-data/dhi_ndvi_2015.tif') %>%
+  aggregate(4) %>%
+  project(r_s2)
+#' **USING THE KEYS ONE FOR TESTS BECAUSE IT'S FASTER**
+r_hfi <- rast('data/other-rasters/hfi-layers/ml_hfi_v1_2019.nc') %>%
+  project(r_s2)
+# r_hfi <- rast('H:/GitHub/ndvi-stochasticity/data/other-rasters/hfp_2021_100m_v1-2_cog.tif')
 r_rich <- rast('data/other-rasters/iucn-red-list-spp-richness/Combined_SR_2024.tif') %>%
   project(crs(r_s2))
 if(file.exists('data/other-rasters/FireCCI51based/global_monthly_burned_area_fraction_05deg_caluclated-average.nc')) {
@@ -30,7 +36,18 @@ if(file.exists('data/other-rasters/FireCCI51based/global_monthly_burned_area_fra
   writeCDF(r_burn, 'data/other-rasters/FireCCI51based/global_monthly_burned_area_fraction_05deg_caluclated-average.nc')
 }
 
-# some rasters have a difference CRS, but reprojecting takes too long
+r_precip <- rast('data/precipitation-yearly-mean-m-day.tif') %>%
+  project(r_s2) %>%
+  mask(ecoregions)
+
+#' *data that might be worth adding:*
+#' - temperature: `https://cds.climate.copernicus.eu/datasets/derived-near-surface-meteorological-variables?tab=download` (need to make a raster)
+#' - seasonal temperature range (need to make a raster)
+#' - max body size (can't find a raster)
+#' - invasive species (can't find a raster)
+#' - disease occurrence? (can't find a raster)
+
+# some have a difference CRS, but reprojecting the HFI layer takes too long
 tibble(raster = c('r_s2', 'r_mu', 'r_dhi', 'r_hfi', 'r_rich', 'r_burn'),
        crs = sapply(raster, \(x) crs(get(x), proj = TRUE)),
        same_crs = crs == crs[1])
@@ -43,6 +60,7 @@ if(FALSE) {
   plot(r_hfi)
   plot(r_rich)
   plot(r_burn)
+  plot(r_precip)
 }
 
 get_values <- function(rst, pts) {
@@ -59,7 +77,9 @@ d <- as.data.frame(r_mu, xy = TRUE) %>%
          s2_hat = get_values(r_s2, .),
          hfi = get_values(r_hfi, .) / 1e3, # scale back to [0, 50]
          richness = get_values(r_rich, .),
-         burned = get_values(r_burn, .)) %>%
+         burned = get_values(r_burn, .),
+         precip_m_year = get_values(r_precip, .) * 365) %>%
+  # DHI rasters need to be extracted separately
   bind_cols(.,
             extract(r_dhi, select(., x, y) %>%
                       vect(geom = c('x', 'y')) %>%
@@ -72,18 +92,24 @@ d <- as.data.frame(r_mu, xy = TRUE) %>%
   as_tibble()
 d
 
-d %>%
-  filter(mu_hat > -0.25) %>% #' *remove: there were 4 points mean < -0.25*
+#' no `geom_smooth()` because the computation is too slow
+fig_3 <-
+  d %>%
+  mutate(precip_m_year = if_else(precip_m_year > 5, 5, precip_m_year)) %>%
+  #' *remove: there were 4 points mean < -0.25*----------------------------
+  filter(mu_hat > -0.25) %>%
   tidyr::pivot_longer(- c(x, y, s2_hat), names_to = 'variable',
                       values_to = 'value') %>%
-  mutate(lab = case_when(variable == 'mu_hat' ~ 'Estimated mean NDVI',
-                         variable == 'hfi' ~ 'Human footprint',
-                         variable == 'richness' ~ 'Species richness',
-                         variable == 'dhi_cumulative' ~ 'Cumulative NDVI',
-                         variable == 'dhi_min' ~ 'Minimum NDVI',
-                         variable == 'dhi_seasonal' ~ 'Seasonal CV of NDVI',
-                         variable == 'burned' ~ 'Mean proportion buned') %>%
-           factor(., levels = unique(.))) %>%
+  mutate(lab = case_when(
+    variable == 'mu_hat' ~ 'Estimated mean NDVI',
+    variable == 'hfi' ~ 'Human footprint',
+    variable == 'richness' ~ 'Species richness',
+    variable == 'dhi_cumulative' ~ 'Cumulative NDVI',
+    variable == 'dhi_min' ~ 'Minimum NDVI',
+    variable == 'dhi_seasonal' ~ 'Seasonal CV of NDVI',
+    variable == 'burned' ~ 'Mean proportion buned',
+    variable == 'precip_m_year' ~ 'Annual precipitation (m)') %>%
+      factor(., levels = unique(.))) %>%
   ggplot() +
   facet_wrap(~ lab, scales = 'free_y', strip.position = 'left', nrow = 2) +
   geom_hex(aes(s2_hat, value, fill = log10(after_stat(count))),
@@ -93,10 +119,9 @@ d %>%
                             bold(' scale)'))), range = c(0, 1),
     reverse = FALSE, labels = \(.x) 10^.x) +
   labs(y = NULL, x = 'DENVar') +
-  theme(legend.position = 'inside', legend.position.inside = c(7/8, 0.25),
-        strip.background = element_blank(),
-        strip.placement = 'outside',# legend.key.width = unit(0.5, 'in'),
+  theme(strip.background = element_blank(), strip.placement = 'outside',
+        legend.position = 'top', legend.key.width = rel(2),
         strip.text = element_text(size = rel(1)))
 
-ggsave('figures/hexplots-global-test.png',
+ggsave('figures/fig-3.png', fig_3,
        width = 12, height = 5, units = 'in', dpi = 600, bg = 'white')
