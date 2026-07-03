01be44b6fa6f311f5284a730abd0cc9bb2b91767
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2026-07-02 20:22:30 -0700

    improved scales of fig 3

--- analysis/figures/010-fig-3.R
+++ analysis/figures/010-fig-3.R
@@ -11,19 +11,22 @@ d <- readr::read_csv('output/yearly-estimates.csv', num_threads = 10) %>%
             .by = c(year, y))
 
 mean(d$s2_hat > 0.05) # < 1%
-d <- mutate(d,
-            s2_hat = if_else(s2_hat > 0.05, 0.05, s2_hat),
-            ref_mu = mean(mu_hat[year > 1990 & year <= 2000]),
-            ref_s2 = mean(s2_hat[year > 1990 & year <= 2000]),
-            diff_mu = mu_hat - ref_mu,
-            diff_s2 = s2_hat - ref_s2,
-            .by = y)
+d <- d %>%
+  mutate(
+    s2_hat = if_else(s2_hat > 0.05, 0.05, s2_hat),
+    ref_mu = mean(mu_hat[year > 1990 & year <= 2000]),
+    ref_s2 = mean(s2_hat[year > 1990 & year <= 2000]),
+    diff_mu = mu_hat - ref_mu,
+    diff_s2 = s2_hat - ref_s2,
+    diff_mu = if_else(abs(diff_mu) > 0.15, sign(diff_mu) * 0.15, diff_mu),
+    diff_s2 = if_else(abs(diff_s2) > 0.02, sign(diff_s2) * 0.02, diff_s2),
+    .by = y)
 
 fig_3 <- plot_grid(
   ggplot(d, aes(year, y)) +
     geom_raster(aes(fill = mu_hat)) +
     geom_contour(aes(year, y, z = mu_hat), color = '#00000080', bins = 8) +
-    scale_x_continuous(expand = c(0, 0)) +
+    scale_x_continuous(NULL, breaks = seq(1985, 2025, by = 5), expand = c(0, 0)) +
     scale_y_continuous(expand = c(0, 0)) +
     labs(x = 'Year', y = 'Latitude (degrees N)') +
     scale_fill_gradientn('Mean NDVI   ', colours = ndvi_pal,
@@ -33,7 +36,7 @@ fig_3 <- plot_grid(
   ggplot(d, aes(year, y)) +
     geom_raster(aes(fill = s2_hat)) +
     geom_contour(aes(z = s2_hat), color = '#00000080', bins = 8) +
-    scale_x_continuous(expand = c(0, 0)) +
+    scale_x_continuous(NULL, breaks = seq(1985, 2025, by = 5), expand = c(0, 0)) +
     scale_y_continuous(expand = c(0, 0)) +
     scale_fill_davos(name = 'DENVar ', limits = c(0, NA), reverse = TRUE) +
     labs(x = 'Year', y = 'Latitude (degrees N)') +
@@ -41,8 +44,8 @@ fig_3 <- plot_grid(
           legend.title = element_text(hjust = 0)),
   ggplot(d, aes(year, y)) +
     geom_raster(aes(fill = diff_mu)) +
-   geom_contour(aes(year, y, z = diff_mu), color = '#00000080', bins = 8) +
-    scale_x_continuous(expand = c(0, 0)) +
+    geom_contour(aes(year, y, z = diff_mu), color = '#00000080', bins = 8) +
+    scale_x_continuous(NULL, breaks = seq(1985, 2025, by = 5), expand = c(0, 0)) +
     scale_y_continuous(expand = c(0, 0)) +
     labs(x = 'Year', y = 'Latitude (degrees N)') +
     scale_fill_vik(name = 'Change in mean NDVI      ', reverse = TRUE) +
@@ -50,8 +53,8 @@ fig_3 <- plot_grid(
           legend.title = element_text(hjust = 0)),
   ggplot(d, aes(year, y)) +
     geom_raster(aes(fill = diff_s2)) +
-   geom_contour(aes(z = diff_s2), color = '#00000080', bins = 8) +
-    scale_x_continuous(expand = c(0, 0)) +
+    geom_contour(aes(z = diff_s2), color = '#00000080', bins = 8) +
+    scale_x_continuous(NULL, breaks = seq(1985, 2025, by = 5), expand = c(0, 0)) +
     scale_y_continuous(expand = c(0, 0)) +
     scale_fill_bam(name = 'Change in DENVar    ', reverse = TRUE) +
     labs(x = 'Year', y = 'Latitude (degrees N)') +
