29edb1f3eaa74cb9bbb15be478429a43cfba9d0e
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2026-07-04 08:46:34 -0700

    saved data for fig 3

--- analysis/figures/010-fig-3.R
+++ analysis/figures/010-fig-3.R
@@ -5,27 +5,34 @@ library('ggplot2') # for fancy plots
 library('cowplot') # for fancy plots with multiple panels
 source('analysis/figures/000-default-ggplot-theme.R')
 
-d <- readr::read_csv('output/yearly-estimates.csv', num_threads = 10) %>%
-  summarize(mu_hat = mean(mu_hat, na.rm = TRUE),
-            s2_hat = mean(s2_hat, na.rm = TRUE),
-            .by = c(year, y))
-
-mean(d$s2_hat > 0.05) # < 1%
-d <- d %>%
-  mutate(
-    s2_hat = if_else(s2_hat > 0.05, 0.05, s2_hat),
-    ref_mu = mean(mu_hat[year > 1990 & year <= 2000]),
-    ref_s2 = mean(s2_hat[year > 1990 & year <= 2000]),
-    diff_mu = mu_hat - ref_mu,
-    diff_s2 = s2_hat - ref_s2,
-    diff_mu = if_else(abs(diff_mu) > 0.1, sign(diff_mu) * 0.1, diff_mu),
-    diff_s2 = if_else(abs(diff_s2) > 0.01, sign(diff_s2) * 0.01, diff_s2),
-    .by = y)
+if (file.exists('output/yearly-latitudinal-change.rds')) {
+  d <- readRDS('output/yearly-latitudinal-change.rds')
+} else {
+  d <- readr::read_csv('output/yearly-estimates.csv', num_threads = 10) %>%
+    summarize(mu_hat = mean(mu_hat, na.rm = TRUE),
+              s2_hat = mean(s2_hat, na.rm = TRUE),
+              .by = c(year, y))
+  
+  mean(d$s2_hat > 0.05) # < 1%
+  d <- d %>%
+    mutate(
+      s2_hat = if_else(s2_hat > 0.05, 0.05, s2_hat),
+      ref_mu = mean(mu_hat[year > 1990 & year <= 2000]),
+      ref_s2 = mean(s2_hat[year > 1990 & year <= 2000]),
+      diff_mu = mu_hat - ref_mu,
+      diff_s2 = s2_hat - ref_s2,
+      diff_mu = if_else(abs(diff_mu) > 0.1, sign(diff_mu) * 0.1, diff_mu),
+      diff_s2 = if_else(abs(diff_s2) > 0.01, sign(diff_s2) * 0.01, diff_s2),
+      .by = y)
+  saveRDS(d, 'output/yearly-latitudinal-change.rds')
+}
 
 fig_3 <- plot_grid(
   ggplot(d, aes(year, y)) +
     geom_raster(aes(fill = mu_hat)) +
     geom_contour(aes(year, y, z = mu_hat), color = '#00000080', bins = 8) +
+    geom_vline(xintercept = c(1982, 1997, 2015), lty = 'dashed') +
+    geom_vline(xintercept = c(), lty = 'dashed') +
     scale_x_continuous(NULL, breaks = seq(1985, 2025, by = 5), expand = c(0, 0)) +
     scale_y_continuous(expand = c(0, 0)) +
     labs(x = 'Year', y = 'Latitude (degrees N)') +
